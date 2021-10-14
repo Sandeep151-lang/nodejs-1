593283769b36c232733f9fa0efac4b0eb3350f48
Sandeep <sandeepnandanwar92@gmail.com>
2021-10-14 18:09:56 +0530

    second commit

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const fs = require('fs');
 var http= require('http')
 http.createServer((req, res) => {
     let date = new Date();
+    //write the date and time in file
     fs.writeFileSync("currentDateTime.txt", `${date}`);
+
+    //read form the file
     const data = fs.readFileSync("currentDateTime.txt", 'utf8');
+
+    //giving the response
     res.write(data);
     res.end();
 }).listen(8000);
